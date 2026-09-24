bef5c0728b13290961836bfb999522e34b3393c0
fabian torres <frtt07@gmail.com>
2026-09-23 20:54:57 -0500

    nombre para zona

--- backend/app/domain/zone.py
+++ backend/app/domain/zone.py
@@ -1,6 +1,8 @@
 class Zone:
 
-    def __init__(self, x_min, x_max, y_min, y_max, is_populated):
+    def __init__(self,name,  x_min, x_max, y_min, y_max, is_populated):
+
+        self.name = name
         self.x_min = x_min
         self.x_max = x_max
         self.y_min = y_min
